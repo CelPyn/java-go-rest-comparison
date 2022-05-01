5f7b6c3c8f5c612bc8e741850e9fc5dc0623d856
Cel Pynenborg <celpynenborg@gmail.com>
2022-05-01 17:02:27 +0200

    Remove implementations

--- go/main.go
+++ go/main.go
@@ -1,54 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"time"
-)
-
-type message struct {
-	Content string    `json:"content"`
-	Time    time.Time `json:"time"`
-}
-
 func main() {
 
-	mux := http.NewServeMux()
-	mux.Handle("/hello", loggingHandler(Get(helloHandler(nil))))
-
-	log.Println("Listening...")
-	log.Fatal(http.ListenAndServe(":8080", mux))
-}
-
-func loggingHandler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(fmt.Sprintf("%s - %s", r.Method, r.URL))
-		next.ServeHTTP(w, r)
-	})
-}
-
-func Get(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func helloHandler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		message := message{
-			Content: "Hello!",
-			Time:    time.Now(),
-		}
-		res, _ := json.Marshal(message)
-		fmt.Fprintf(w, string(res))
-		if next != nil {
-			next.ServeHTTP(w, r)
-		}
-	})
 }
--- java/src/com/axxes/demo/Main.java
+++ java/src/com/axxes/demo/Main.java
@@ -1,52 +1,9 @@
 package com.axxes.demo;
 
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.time.LocalDateTime;
-import java.util.concurrent.Executors;
-
 public class Main {
 
     public static void main(String[] args) {
-        HttpServer server = createServer();
-        server.setExecutor(Executors.newFixedThreadPool(10));
-        server.createContext("/hello", helloWorldHandler());
-        server.start();
-        System.out.println("Server started");
+
     }
 
-    private static HttpServer createServer() {
-        try {
-            return HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static HttpHandler helloWorldHandler() {
-        return exchange -> {
-            if (!"GET".equals(exchange.getRequestMethod())) {
-                exchange.sendResponseHeaders(404, 0);
-            }
-
-            try (OutputStream os = exchange.getResponseBody()) {
-                Message message = new Message("Hello World", LocalDateTime.now());
-
-                String response = "{\n" +
-                        "\"content:\": \"" + message.content() + "\"," +
-                        "\"time:\": \"" + message.time() + "\"" +
-                        "}";
-                exchange.sendResponseHeaders(200, response.length());
-                os.write(response.getBytes());
-                os.flush();
-            }
-        };
-    }
-
-    private record Message(String content, LocalDateTime time) {
-    }
 }
